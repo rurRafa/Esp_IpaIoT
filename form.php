23589e0aac9cd7a3d641f3be3405408d14cd1a34
rurRafa <114644451+rurRafa@users.noreply.github.com>
2022-11-19 12:57:46 +0100

    p

--- form.php
+++ form.php
@@ -2,22 +2,22 @@
     extract($_REQUEST);
     $file=fopen("saveform.txt","a");
 
-    fwrite($file,"name :");
+    fwrite($file,"name: ");
     fwrite($file, $name ."\n");
 
-    fwrite($file,"surname :");
+    fwrite($file,"surname: ");
     fwrite($file, $surname ."\n");
 
-    fwrite($file,"number :");
+    fwrite($file,"number: ");
     fwrite($file, $number ."\n");
 
-    fwrite($file,"email :");
+    fwrite($file,"email: ");
     fwrite($file, $email ."\n");
 
-    fwrite($file,"password :");
+    fwrite($file,"password: ");
     fwrite($file, $password ."\n");
 
-    fwrite($file,"repassword :");
+    fwrite($file,"repassword: ");
     fwrite($file, $repassword ."\n");
 
     fclose($file);
