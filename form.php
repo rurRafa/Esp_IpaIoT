c1ba043aa272a11e5515805c4a9c4da1f2cc902e
rurRafa <114644451+rurRafa@users.noreply.github.com>
2022-11-20 19:54:49 +0100

    p

--- form.php
+++ form.php
@@ -1,10 +1,8 @@
 <?php
-      $sn1 = $_GET["a"];
-      $sn2 = $_GET["b"];
+      $sn1 = $_GET["name"];
       
-      $file1 = fopen("sensors.txt","a") or die("Unable to open file!");
-      $text1 = "a=" . $sn1 . " b=" . $sn2;
-      
+      $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+      $text1 = "name=" . $sn1    
       fwrite($file1, $text1);
       fclose($file1);
  ?>
