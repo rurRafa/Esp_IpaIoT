e3d549fa2bbda52c3409ea66cdfeb9e5c9ba8872
rurRafa <114644451+rurRafa@users.noreply.github.com>
2022-11-20 19:46:40 +0100

    p

--- form.php
+++ form.php
@@ -1,20 +1,25 @@
 <?php
-    $sn1 = $_GET["a"];
-    $sn2 = $_GET["b"];
-    
-    $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
-    $text1 = "a=" . $sn1 . " b=" . $sn2;
-    
-    fwrite($file1, $text1);
-    fclose($file1);
-    
-    $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
-    $text2 = "Value from actuator. Save this value to actuator.txt";
-    fwrite($file2, $text2);
-    fclose($file2);
-    
-    $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
-    $text3 = fread($file3,filesize("actuator.txt"));
-    echo $text3;
-    fclose($file3);
+    extract($_REQUEST);
+    $file=fopen("saveform.txt","a");
+
+    fwrite($file,"name: ");
+    fwrite($file, $name ."\n");
+
+    fwrite($file,"surname: ");
+    fwrite($file, $surname ."\n");
+
+    fwrite($file,"number: ");
+    fwrite($file, $number ."\n");
+
+    fwrite($file,"email: ");
+    fwrite($file, $email ."\n");
+
+    fwrite($file,"password: ");
+    fwrite($file, $password ."\n");
+
+    fwrite($file,"repassword: ");
+    fwrite($file, $repassword ."\n");
+
+    fclose($file);
+    header("location: index.php?form=sent");
  ?>
